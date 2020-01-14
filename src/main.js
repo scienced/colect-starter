a5bc253eea52c17cc365db386bb4201e82242d78
scienced <michiel@Michiels-Air.fritz.box>
2020-01-14 08:15:46 +0100

    added fontawesome

--- src/main.js
+++ src/main.js
@@ -4,10 +4,31 @@ import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
-import Buefy from 'buefy'
+//import Buefy from 'buefy'
 //import 'buefy/dist/buefy.css'
 
-Vue.use(Buefy)
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+// internal icons
+import { faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+    faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faEdit, faPlusCircle, faEllipsisV, faTrashAlt } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faCheck, faCheckCircle, faInfoCircle, faExclamationTriangle, faExclamationCircle,
+    faArrowUp, faAngleRight, faAngleLeft, faAngleDown,
+    faEye, faEyeSlash, faCaretDown, faCaretUp, faUpload, faEdit, faPlusCircle, faEllipsisV, faTrashAlt);
+Vue.component('vue-fontawesome', FontAwesomeIcon);
+
+
+import Buefy from 'buefy'
+Vue.use(Buefy, {
+  defaultIconComponent: 'vue-fontawesome',
+  defaultIconPack: 'fas',
+});
+
+
+//Vue.use(Buefy)
 
 new Vue({
   router,
